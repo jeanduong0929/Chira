4b3e82d69a1f182cf66069dff366de553b1717b3
Jean Duong <baoduongxy@gmail.com>
2024-11-30 14:03:12 -0500

    feat(navbar): add a create button and user button to the navbar for enhanced user interaction style(button): update button color to a specific hex value for consistent branding and improved visibility

--- src/app/(protected)/_components/navbar.tsx
+++ src/app/(protected)/_components/navbar.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { ProjectDropdown } from "./project-dropdown";
 
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
 export const Navbar = () => {
   return (
     <div className="h-[56px] w-full flex items-center border-b">
       <nav className="max-w-screen-2xl w-11/12 mx-auto flex items-center justify-between">
-        <ProjectDropdown />
+        <div>
+          <ProjectDropdown />
+          <Button>Create</Button>
+        </div>
+
+        <UserButton />
       </nav>
     </div>
   );
