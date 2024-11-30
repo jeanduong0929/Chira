1c9364509d3062ecfae4d6199698c78e3b598e28
Jean Duong <baoduongxy@gmail.com>
2024-11-30 13:52:12 -0500

    feat(layout.tsx): create ProtectedLayout component to wrap protected routes with a consistent layout structure

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ProtectedLayoutProps {
+  children: React.ReactNode;
+}
+
+const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default ProtectedLayout;
