0ace2124e158c3185869de3833da27ca4463e501
Jean Duong <baoduongxy@gmail.com>
2024-12-06 13:38:02 -0500

    feat(issue-type-select): add IssueTypeSelect component for selecting issue types feat(priority-select): add PrioritySelect component for selecting priority levels refactor(sprint-edit-issue-dialog): integrate IssueTypeSelect and PrioritySelect components to streamline issue editing process

---
+++ src/app/(protected)/projects/[projectId]/backlog/_components/issue-type-select.tsx
@@ -0,0 +1,57 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { Bookmark, CheckIcon, Circle } from "lucide-react";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const IssueTypeSelect = ({
+  issueType,
+  setIssueType,
+}: {
+  issueType: "story" | "bug" | "task";
+  setIssueType: Dispatch<SetStateAction<"story" | "bug" | "task">>;
+}) => {
+  return (
+    <Select
+      defaultValue={issueType}
+      onValueChange={(value) => {
+        setIssueType(value as "story" | "bug" | "task");
+      }}
+    >
+      <SelectTrigger className="w-[350px]">
+        <SelectValue placeholder="Bug" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem key="story" value="story">
+          <div className="flex items-center gap-x-3">
+            <div className="shrink-0 bg-[#64BA3B] p-1">
+              <Bookmark className="size-3" fill="#fff" stroke="#fff" />
+            </div>
+            <span>Story</span>
+          </div>
+        </SelectItem>
+        <SelectItem key="task" value="task">
+          <div className="flex items-center gap-x-3">
+            <div className="shrink-0 bg-[#0B66E4] p-1">
+              <CheckIcon className="size-3 text-white" strokeWidth={4} />
+            </div>
+            <span>Task</span>
+          </div>
+        </SelectItem>
+        <SelectItem key="bug" value="bug">
+          <div className="flex items-center gap-x-3">
+            <div className="shrink-0 bg-[#E84C3D] p-1">
+              <Circle className="size-3" fill="#fff" stroke="#fff" />
+            </div>
+            <span>Bug</span>
+          </div>
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
---
+++ src/app/(protected)/projects/[projectId]/backlog/_components/priority-select.tsx
@@ -0,0 +1,50 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const PrioritySelect = ({
+  priority,
+  setPriority,
+}: {
+  priority: "low" | "medium" | "high";
+  setPriority: Dispatch<SetStateAction<"low" | "medium" | "high">>;
+}) => {
+  return (
+    <Select
+      defaultValue={priority}
+      onValueChange={(value) => {
+        setPriority(value as "low" | "medium" | "high");
+      }}
+    >
+      <SelectTrigger className="w-[350px]">
+        <SelectValue placeholder="Bug" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem key="low" value="low">
+          <div className="flex items-center gap-x-3">
+            <div className="size-3 shrink-0 bg-[#64BA3B]" />
+            <span>Low</span>
+          </div>
+        </SelectItem>
+        <SelectItem key="medium" value="medium">
+          <div className="flex items-center gap-x-3">
+            <div className="size-3 shrink-0 bg-[#0B66E4]" />
+            <span>Medium</span>
+          </div>
+        </SelectItem>
+        <SelectItem key="high" value="high">
+          <div className="flex items-center gap-x-3">
+            <div className="size-3 shrink-0 bg-[#E84C3D]" />
+            <span>High</span>
+          </div>
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
