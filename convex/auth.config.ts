0b149f450f44930ff8e6c60ad78990a0bdc0a817
Jean Duong <baoduongxy@gmail.com>
2024-12-13 11:34:10 -0500

    fix(auth.config.ts): replace hardcoded domain with environment variable CLERK_ISSUER_URL for better configuration management

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const config = {
   providers: [
     {
-      domain: "https://above-zebra-71.clerk.accounts.dev",
+      domain: process.env.CLERK_ISSUER_URL!,
       applicationID: "convex",
     },
   ],
